e01ed1a33ec94c4396971a671049cd115b5cab76
soyboon <lim_soy_boon@ida.gov.sg>
2014-10-24 23:30:27 +0800

    Getting and Cleaning Data Project Code
    Removed bug and streamlined the column naming code.

--- run_analysis.R
+++ run_analysis.R
@@ -14,19 +14,8 @@ X_test <- read.table("X_test.txt")
 y_train <- read.table("y_train.txt")
 y_test <- read.table("y_test.txt")
 
-## Bind training and testing data into one data frame
+## Bind training and testing measurement data into one data frame
 X_all <- rbind(X_train, X_test)
-
-## Assign column names
-col_names <- as.vector(features[,2])
-colnames(X_all) <- col_names
-
-## Remove unwanted columns - leave only those with "mean" or "std"
-col_include <- subset(features, grepl("mean\\(\\)", features[,2]) | 
-                            grepl("std\\(\\)", features[,2]), select=V2)
-col_include <- as.vector(col_include[,1])
-X_all <- subset(X_all, select=col_include)
-
 
 ## Bind training and testing subject data into one data frame
 subject_all <- rbind(subject_train, subject_test)
@@ -34,36 +23,40 @@ subject_all <- rbind(subject_train, subject_test)
 ## Bind training and testing activity data into one data frame
 ## and convert to actual activity names
 y_all <- rbind(y_train, y_test)
-y_all <- merge(y_all, activity_labels, sort = FALSE)
-
+y_all <- data.frame(activity_labels[match(y_all$V1, activity_labels$V1), 2])
 
 ## Bind data + subject + activity together into one data frame
-result <- cbind(X_all, subject_all, y_all[,2])
-col_include <- c(col_include, "subject", "activity")
-colnames(result) <- col_include
+## and remove special characters (except "-") in the column names
+result <- cbind(X_all, subject_all, y_all)
+
+## Assign column names
+col_names <- as.vector(features[,2])
+colnames(result) <- c(col_names, "subject", "activity")
 
 
-###################################
-###    Step 1 to 4 Completed    ###
-###################################
-View(result)
-
+## Remove unwanted columns - leave only those with "mean" or "std"
+col_include <- subset(features, grepl("mean\\(\\)", features[,2]) | 
+                          grepl("std\\(\\)", features[,2]), select=V2)
+col_include <- as.vector(col_include[,1])
+result <- subset(result, select=c(col_include, "subject", "activity"))
+col_include <- gsub("[^0-9A-Za-z///' //-]", "", col_include)
+colnames(result) <- c(col_include, "subject", "activity")
 
 
 ## create new data table from the result data frame
 ## calculate the mean for each measurement grouped by subject and activity
 result_mean <- data.table(result)
 result_mean <- result_mean[, lapply(.SD,mean), by=c("subject", "activity")]
+result_mean <- result_mean[order(subject),]
 
-## Rename columns to add a prefix "avg" 
-setnames(result_mean, unlist(lapply("avg", paste, colnames(result_mean), sep = "-")))
-setnames(result_mean, c("avg-subject", "avg-activity"), c("subject", "activity"))
 
 ## Write data to file
 ## CSV: write.csv(result_mean, file="avg_data.csv")
 write.table(result_mean, file="avg_data.txt", row.name=FALSE)
 
-########################
-### Step 5 Completed ###
-########################
 
+
+###################################
+## code to read back result file ##
+###################################
+## final_result <- read.table("avg_data.txt", header = TRUE)
